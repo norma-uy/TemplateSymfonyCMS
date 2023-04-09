f0f153e46f26f2da29022cb4e6c65d65b0be2530
Samuel Alvarez <42385434+samuelV102@users.noreply.github.com>
2023-04-09 17:57:31 -0300

    API for media resource twig templates Fixes #5

--- config/bundles.php
+++ config/bundles.php
@@ -19,4 +19,5 @@ return [
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
     League\FlysystemBundle\FlysystemBundle::class => ['all' => true],
+    Liip\ImagineBundle\LiipImagineBundle::class => ['all' => true],
 ];
